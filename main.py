0c960f0c7e7c977725f940428cb6dec892861375
Kyle Murphy <murphy.kyle.r@gmail.com>
2025-02-24 13:51:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,40 @@ import matplotlib.patches as mpatches
 
 from time import gmtime, strftime
 
+E_ID = os.environ['E_ID']
+RPLY_ID = os.environ['RPLY_ID']
+
+def sc2replaystats_df(game_dat):
+
+    df = pd.DataFrame(game_dat)
+
+    dat_keys = ['race','mmr','division','apm','team','winner']
+    opp = []
+    upa = []
+
+
+    for index, row in df.iterrows():
+        players = dict()
+        for p, n in zip(row['players'],np.arange(len(df.loc[0,'players']))):
+            player = dict()
+            player['id'] = p['player']['battle_tag_name']
+            for k in dat_keys:
+                player[k] = p[k]
+
+            if player['id'] == 'UpATree':
+                upa.append(player)
+            else:
+                opp.append(player)
+
+    u_df = pd.DataFrame(upa)
+    o_df = pd.DataFrame(opp)
+
+    u_df = u_df.join(o_df, lsuffix='_UpATree', rsuffix='_Opp')
+
+    df = df.join(u_df)
+
+    return df
+
 def generate_streak_info(df, column='winner_UpATree'):
     data = df[column].to_frame()
     data['start_of_streak'] = data[column].ne(data[column].shift())
@@ -19,6 +53,30 @@ def generate_streak_info(df, column='winner_UpATree'):
     data['streak_counter'] = data.groupby('streak_id').cumcount() + 1
     shots_with_streaks = pd.concat([df, data['streak_counter']], axis=1)
     return shots_with_streaks
+
+tk_res = requests.post('https://api.sc2replaystats.com/account/login', 
+                       data = {'email_address':E_ID, 'password':RPLY_ID})
+tk_header = {'Authorization': f'{tk}'}
+
+# make initial request
+res = requests.get('https://api.sc2replaystats.com/player/1384133/replays/62', headers=tk_header)
+
+# determine the number of pages we are looking at
+npages = res.json()['total_items'] // res.json()['items_per_page'] 
+if res.json()['total_items'] % res.json()['items_per_page'] > 1: npages = npages + 1
+
+# load the last page and put data into dataframe
+p_res = requests.get('https://api.sc2replaystats.com/player/1384133/replays/62', 
+                     headers=tk_header, params={'page': npages})
+if p_res.status_code == 200:
+    gm_df = sc2replaystats_df(p_res.json()['items']) 
+    gm_df = gm_df.drop(columns='players') 
+    game_df = pd.read_csv('UpATree.csv.gz')
+    game_df = game_df.drop(columns='Unnamed: 0')
+    game_df = pd.concat([game_df,gm_df], ignore_index=True)
+    game_df = game_df.drop_duplicates(subset='replay_url')
+    game_df.to_csv('UpATree.csv.gz', mode='a',compression='gzip')
+
 
 # load subathon data that was retrieved from sc2replaystats
 # and read the game columns
